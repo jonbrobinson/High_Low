19c8f560865f47af7318077b70c70ab08b373d65
jonathan robinson <jonbrobinson@gmail.com>
2014-05-12 11:47:44 -0500

    Finished simple working version of HL game. V1

--- highlow.php
+++ highlow.php
@@ -1,9 +1,32 @@
 <?php
 
 //Get User Name
+
+$Win_num = rand(1,100);
+
 fwrite(STDOUT, 'What is your first name? ');
 $first_name = fgets(STDIN);
 fwrite(STDOUT, "Hello $first_name" . "I'm thinking of a number between 1 and 100.\nCan you guess which one it is?\n");
+
+$user_guess = fgets(STDIN);
+
+while ($user_guess != $Win_num) {
+	
+	if ($user_guess < $Win_num) {
+		echo "Higher" ;
+		$user_guess = fgets(STDIN);
+
+	}
+	else {
+		echo "Lower" ;
+		$user_guess = fgets(STDIN);
+	}
+
+}
+
+	echo "Winner Winner, Chicken Dinner" ;
+
+
 
 //Computer Generates Number
 //User will guess number
